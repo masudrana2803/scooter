019b5d9d70c1c7ebaa0c7492e84ae461a0ea2eb1
masudrana2803 <masudrana2803@gmail.com>
2025-06-21 02:46:05 +0600

    edit-part

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@
     delay: 10,
     time: 1000
 });
+
+// slider part 1
+ $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  // dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+});
+			
+
+
+// slide 2
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+});
+		
